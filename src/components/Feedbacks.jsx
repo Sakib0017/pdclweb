b550b6c6921f8a7f7d09a514e144f137cda86d50
Sakib0017 <sakibkamal0017@gmail.com>
2024-02-18 16:09:48 +0600

    Updated one  P

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -54,7 +54,6 @@ const Feedbacks = () => {
         <motion.div variants={textVariant()}>
           
           <h2 className=" text-Start text-gray-900/50 font-medium font-ubuntu text-[28px]">TESTIMONIALS</h2>
-          <h2 className=" text-Start text-gray-900/50 font-medium font-ubuntu text-[28px]">TESTIMONIALS</h2>
          
         </motion.div>
       </div>
