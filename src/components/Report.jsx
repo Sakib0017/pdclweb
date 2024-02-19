2ccb9ea28f9dbd003addf5896af7725caad93ef2
Sakib0017 <sakibkamal0017@gmail.com>
2024-02-19 20:31:55 +0600

    Updated new Pages

--- src/components/Report.jsx
+++ src/components/Report.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import {   Nav, Navbar,  Tech} from "../components";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
@@ -7,106 +7,105 @@ import { SectionWrapper } from "../hoc";
 import { projects1  } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 import { Card, Typography, IconButton } from "@material-tailwind/react";
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
  
-const TABLE_HEAD = ["Test Name", "Pataint Name", "Publish Date", "Download", "Print"];
- 
-const TABLE_ROWS = [
-  {
-    name: "Blood Test",
-    job: "Rayhan Ali",
-    date: "23/04/23",
-    icon1:downloads,
-    icon2:printer,
-  },
-  {
-    name: "Urine Test",
-    job: "Rayhan Ali",
-    date: "23/04/23",
-    icon1:downloads,
-    icon2:printer,
-  },
-  {
-    name: "Tssue Cell Test",
-    job: "Rayhan Ali",
-    date: "19/09/23",
-    icon1:downloads,
-    icon2:printer,
-  },
-  {
-    name: "Dengue Test",
-    job: "Rayhan Ali",
-    date: "24/12/23",
-    icon1:downloads,
-    icon2:printer,
-  },
-  {
-    name: "Corora Test",
-    job: "Rayhan Ali",
-    date: "04/10/23",
-    icon1:downloads,
-    icon2:printer,
-  },
-];
+
 function Report() {
+  const pdfRef = useRef();
+  const downloadPDF = () =>{
+    const input = pdfRef.current;
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4', true);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = pdf.internal.pageSize.getHeight();
+      const imgWidth = canvas.width;
+      const imgHeight = canvas.height;
+      const ratio = Math.min(pdfWidth / imgWidth, pdfHeight / imgHeight);
+      const imgX  = (pdfWidth - imgWidth * ratio ) / 2 ;
+      const imgY = 30;
+      pdf.addImage(imgData, 'PNG', imgX, imgY, imgWidth * ratio, imgHeight * ratio);
+      pdf.save('invoice.pdf');
+    });
+  };
   return (
-    <div className="bg-white">
+    <>
+    <div className="bg-white" >
     <Nav />
      
       
      <Navbar />
  
-  <div className="flex flex-col pt-[200px] mx-auto   max-w-7xl">
+  <div className="flex flex-col pt-[200px] mx-auto   max-w-7xl" >
         <h2 className='text-gray-900/50 pb-5 text-center pl-2 text-[28px] font-bold font-ubuntu'>ALL THE REPORTS THAT YOU WANT</h2>
         </div>
     
-        <div className="flex flex-col pb-10 mx-auto p-3  max-w-7xl">
-        <Card className="h-full w-full overflow-scroll bg-gray-100">
-      <table className="w-full min-w-max table-auto  text-left">
+        <div className="flex flex-col pb-10 mx-auto p-3  max-w-7xl" >
+        <Card className="h-full w-full overflow-scroll bg-gray-100" ref={pdfRef}>
+      <table className="w-full min-w-max table-auto  text-left" >
         <thead>
           <tr>
-            {TABLE_HEAD.map((head) => (
-              <th key={head} className="border-b  text-[#006642] border-blue-gray-100 bg-blue-gray-50 p-4">
-                <Typography
-                  variant="bold"
-                  color="blue-gray"
-                  className=" font-bold font-ubuntu leading-none opacity-70"
-                >
-                  {head}
-                </Typography>
+           
+              <th className="border-b  text-[#006642] border-blue-gray-100 bg-blue-gray-50 p-4">
+                
+                 <p>Test Name</p>
+                
               </th>
-            ))}
+              <th className="border-b  text-[#006642] border-blue-gray-100 bg-blue-gray-50 p-4">
+                
+                <p>Pataint Name</p>
+               
+             </th>
+             <th className="border-b  text-[#006642] border-blue-gray-100 bg-blue-gray-50 p-4">
+                
+                <p>Publish Date</p>
+               
+             </th>
+             <th className="border-b  text-[#006642] border-blue-gray-100 bg-blue-gray-50 p-4">
+                
+                <p>Download</p>
+               
+             </th>
+             <th className="border-b  text-[#006642] border-blue-gray-100 bg-blue-gray-50 p-4">
+                
+                <p>Print</p>
+               
+             </th>
+         
           </tr>
         </thead>
         <tbody>
-          {TABLE_ROWS.map(({ name, job, date, icon1, icon2 }, index) => (
-            <tr key={name} className="even:bg-blue-gray-50/50">
+          
+            <tr  className="even:bg-blue-gray-50/50">
               <td className="p-4  text-[#006642] font-ubuntu">
                
-                  {name}
+                  <p>Tssue Cell Test</p>
               
               </td>
               <td className="p-4  text-[#006642] font-ubuntu">
                
-                  {job}
+              <p>Rayhan Ali</p>
                
               </td>
               <td className="p-4 text-[#006642] font-ubuntu">
               
-                  {date}
+              <p>19/09/23</p>
                
               </td>
               <td className="p-2">
-               <img className=" h-[18px] w-[18px]" src={icon1} />
+                <button onClick={downloadPDF}> <img className=" h-[18px] w-[18px]"  src={downloads} /></button>
+              
                   
                
               </td>
               <td className="p-2 ">
-               <img className=" h-[18px] w-[18px]" src={icon2} />
+               <img className=" h-[18px] w-[18px]" onClick={downloadPDF} src={printer} />
                   
                
               </td>
             </tr>
-          ))}
+         
         </tbody>
       </table>
     </Card>
@@ -120,7 +119,11 @@ function Report() {
 
      
    </div>
+          
+   </>
   )
 }
+
+
 
 export default Report
